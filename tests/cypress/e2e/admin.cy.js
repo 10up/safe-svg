a3bce017fa262ae25ef077096d72d650698354bb
Dharmesh Patel <dspatel44@gmail.com>
2022-06-03 14:14:55 +0530

    Migrate to cypress 10

---
+++ tests/cypress/e2e/admin.cy.js
@@ -0,0 +1,14 @@
+describe('Admin can login and make sure plugin is activated', () => {
+  before(() => {
+    cy.login();
+  });
+
+  it('Open dashboard', () => {
+    cy.visit(`/wp-admin`);
+    cy.get("h1").should("contain", "Dashboard");
+  });
+
+  it('Can activate plugin if it is deactivated', () => {
+		cy.activatePlugin('safe-svg');
+	});
+});
