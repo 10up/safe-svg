d8bf83fce4f020fb5a16bb0e42da73f568c2bee1
Dharmesh Patel <dspatel44@gmail.com>
2022-07-04 11:59:48 +0530

    Added safe svg tags tests.

---
+++ tests/unit/test-safe-svg-tags.php
@@ -0,0 +1,44 @@
+<?php 
+use \WP_Mock\Tools\TestCase;
+
+/**
+ * SafeSvgTagsTest class tests the safe_svg_tags class and functions.
+ */
+class SafeSvgTagsTest extends TestCase {
+    /**
+	 * Set up our mocked WP functions. Rather than setting up a database we can mock the returns of core WordPress functions.
+	 *
+	 * @return void
+	 */
+	public function setUp(): void {
+		\WP_Mock::setUp();
+	}
+
+    /**
+	 * Tear down WP Mock.
+	 *
+	 * @return void
+	 */
+	public function tearDown(): void {
+		\WP_Mock::tearDown();
+	}
+
+	/**
+	 * Test get tags and filter on it.
+	 *
+	 * @return void
+	 */
+    public function test_get_tags(){
+		$svg_tags = safe_svg_tags::getTags();
+		$this->assertIsArray( $svg_tags );
+
+		$filtered_svg_tags = array_merge( $svg_tags, array( "customTag" ) );
+		\WP_Mock::onFilter( 'svg_allowed_tags' )
+			->with( $svg_tags )
+			->reply( $filtered_svg_tags );
+
+		$svg_tags = safe_svg_tags::getTags();
+		$this->assertContains( 'customTag', $svg_tags );
+		$this->assertEquals( $svg_tags, $filtered_svg_tags );
+    }
+}
