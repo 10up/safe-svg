1d44db582388a059d4cc8c7c862ec608278462de
faisal-alvi <faisalalvi33@gmail.com>
2022-11-14 19:03:33 +0530

    remove the content creation via the save method

--- includes/blocks/safe-svg/save.js
+++ includes/blocks/safe-svg/save.js
@@ -1,40 +1,6 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
 /**
  * @return string Dynamic SVG icon block HTML.
  */
-const SafeSVGBlockSave = ( { attributes } ) => {
-    const {
-        svgURL,
-        imageWidth,
-        imageHeight,
-        dimensionWidth,
-        dimensionHeight,
-        alignment
-    } = attributes;
-    const blockProps = useBlockProps.save();
-    return (
-        <div { ...blockProps }
-             style={{
-                 textAlign: alignment
-             }}
-        >
-            {svgURL &&
-            <svg
-                style={{
-                    width: dimensionWidth,
-                    height: dimensionHeight,
-                }}
-            >
-                <image
-                    xlinkHref={svgURL}
-                    src={svgURL}
-                    width="100%"
-                />
-            </svg>
-            }
-        </div>
-    );
-};
+const SafeSVGBlockSave = () => null;
 
 export default SafeSVGBlockSave;
