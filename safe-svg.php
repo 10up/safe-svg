c14f0d2944e35ab52ea6f29b3b170e0ca701bf23
Viktor Szépe <viktor@szepe.net>
2023-04-10 04:00:41 +0200

    Use absolute path in require

--- safe-svg.php
+++ safe-svg.php
@@ -97,9 +97,9 @@ if ( ! site_meets_php_requirements() ) {
 	return;
 }
 
-require 'includes/safe-svg-tags.php';
-require 'includes/safe-svg-attributes.php';
-require 'includes/blocks.php';
+require __DIR__ . 'includes/safe-svg-tags.php';
+require __DIR__ . 'includes/safe-svg-attributes.php';
+require __DIR__ . 'includes/blocks.php';
 
 if ( ! class_exists( 'safe_svg' ) ) {
 
