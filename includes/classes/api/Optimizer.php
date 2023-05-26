6e494a4b64e6ca7d3ff463bf94ddc42b04ec91bf
Curtis Loisel <curtis.loisel@get10up.com>
2023-05-25 23:26:05 -0500

    Adding interface class

---
+++ includes/classes/api/Optimizer.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Optimizer API Interface
+ *
+ * @package SafeSvg
+ */
+
+namespace SafeSvg\API;
+
+/**
+ * Interface for optmization APIs
+ */
+interface Optimizer {
+
+    public function optimize_image( $image );
+
+}
