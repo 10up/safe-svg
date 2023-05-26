07c17404ad3fdf8efd0651b3663911143d4a38a0
Curtis Loisel <curtis.loisel@get10up.com>
2023-05-25 23:28:08 -0500

    Stubbing out vector express class

---
+++ includes/classes/api/VectorExpress.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Vector Express API
+ *
+ * @package SafeSvg
+ */
+
+namespace SafeSvg\API;
+
+/**
+ * Interface for optmization APIs
+ */
+class VectorExpress implements Optimizer {
+
+    public function optimize_image( $image ) {
+
+    }
+
+}
