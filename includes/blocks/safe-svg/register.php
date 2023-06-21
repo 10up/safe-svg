d2f8c282f9781de07cf6ca05e2ab183f4805877b
Ben Marshall <ben.s.marshall@icloud.com>
2023-06-21 08:06:55 -0500

    fix(classname): fix for the missing custom classname on the svg container in the frontend, see #126

--- includes/blocks/safe-svg/register.php
+++ includes/blocks/safe-svg/register.php
@@ -68,10 +68,11 @@ function render_block_callback( $attributes ) {
 		'safe_svg_inline_markup',
 		sprintf(
 			'<div class="safe-svg-cover" style="text-align:%s">
-				<div class="safe-svg-inside %s" style="width: %spx; height: %spx;">%s</div>
+				<div class="safe-svg-inside %s%s" style="width: %spx; height: %spx;">%s</div>
 			</div>',
 			$attributes['alignment'] ?? 'left',
 			$class_name,
+			isset( $attributes['className'] ) ? ' ' . $attributes['className'] : '',
 			$attributes['dimensionWidth'],
 			$attributes['dimensionHeight'],
 			$contents
